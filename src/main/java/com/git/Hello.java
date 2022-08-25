0a7a5303f552ed6982f67dd4408e317a74aa1685
HaibinLiang <1526167236@qq.com>
2022-08-25 10:49:32 +0800

    第一次更新

---
+++ src/main/java/com/git/Hello.java
@@ -0,0 +1,12 @@
+package com.git;
+
+/**
+ * @author lhb
+ * @date 2022/8/25 10:48
+ * @describe
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
